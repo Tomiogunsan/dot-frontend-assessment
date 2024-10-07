e986daacad248585ff1245e1afdf1526b60946da
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-10-07 16:39:28 +0100

    ch: imolemented addToCart ui

--- src/pages/products/components/AddToCart.tsx
+++ src/pages/products/components/AddToCart.tsx
@@ -1,7 +1,15 @@
 import Drawer from "../../../shared/Drawer";
 
 const AddToCart = ({ onClose }: { onClose: () => void }) => {
-  return <Drawer onClose={onClose} header="Add To Cart">AddToCart</Drawer>;
+  return (
+    <Drawer
+      onClose={onClose}
+      header="Shopping Cart"
+      subHeader="You have 5 items in your cart"
+    >
+      AddToCart
+    </Drawer>
+  );
 };
 
 export default AddToCart;
